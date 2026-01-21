c7569bad1c16d1502bae1402dcc4a884cb78311e
Rushikesh-Gude <rushikeshrdj23@gmail.com>
2026-01-21 16:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const firstname = document.forms[0].fname.value.trim()
+	const lastname = document.forms[0].lname.value.trim()
 
+	alert(firstname + lastname);
+	return false;
 }
